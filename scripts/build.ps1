eb06e46511bfe19ae5fe35ce239290591dfea80a
Ed_ <edwardgz@gmail.com>
2024-07-02 12:22:53 -0400

    WIP: Working toward a unity build of the harfbuzz library

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -8,7 +8,8 @@ $path_win64 = join-path $path_lib  'win64'
 $url_harfbuzz  = 'https://github.com/harfbuzz/harfbuzz.git'
 $path_harfbuzz = join-path $path_root 'harfbuzz'
 
-function build-repo {
+function build-repo
+{
 	verify-path $script:path_lib
 	verify-path $path_win64
 
@@ -62,6 +63,204 @@ function build-repo {
 	write-host "Build completed and files copied to $path_win64"
 }
 build-repo
+
+# NOTE : NOT DONE WIP.
+function Build-RepoWithoutMeson
+{
+    $devshell = join-path $PSScriptRoot 'helpers/devshell.ps1'
+    & $devshell -arch amd64
+
+    verify-path $script:path_lib
+    verify-path $path_win64
+
+    clone-gitrepo $path_harfbuzz $url_harfbuzz
+
+    $path_harfbuzz_build = join-path $path_harfbuzz 'build'
+    verify-path $path_harfbuzz_build
+
+    $library_type = "shared"
+    $build_type   = "release"
+
+    push-location $path_harfbuzz
+
+    $compiler_args = @(
+        "/nologo",
+        "/W3",
+        "/D_CRT_SECURE_NO_WARNINGS",
+        "/DHAVE_FALLBACK=1",
+        "/DHAVE_OT=1",
+        "/DHAVE_SUBSET=1",
+        "/DHB_USE_INTERNAL_PARSER",
+        "/DHB_NO_COLOR",
+        "/DHB_NO_DRAW",
+        "/DHB_NO_PARSE",
+        "/DHB_NO_MT",
+        "/DHB_NO_GRAPHITE2",
+        "/DHB_NO_ICU",
+        "/DHB_NO_DIRECTWRITE",
+        "/I$path_harfbuzz\src",
+        "/I$path_harfbuzz"
+    )
+
+    if ( $library_type -eq "shared" ) {
+        $compiler_args += "/DHAVE_DECLSPEC"
+        $compiler_args += "/DHARFBUZZ_EXPORTS"
+    }
+
+    if ($build_type -eq "debug") {
+        $compiler_args += "/MDd", "/Od", "/Zi"
+    } else {
+        $compiler_args += "/MD", "/O2"
+    }
+
+    $compiler_args = $compiler_args -join " "
+
+    $config_h_content = @"
+#define HB_VERSION_MAJOR 9
+#define HB_VERSION_MINOR 0
+#define HB_VERSION_MICRO 0
+#define HB_VERSION_STRING "9.0.0"
+#define HAVE_ROUND 1
+#define HB_NO_BITMAP 1
+#define HB_NO_CFF 1
+#define HB_NO_SUBSET_CFF 1
+#define HB_HAVE_SUBSET 1
+#define HB_HAVE_OT 1
+#define HB_USER_DATA_KEY_DEFINE1(_name) extern HB_EXTERN hb_user_data_key_t _name
+"@
+
+    Set-Content -Path (Join-Path $path_harfbuzz "config.h") -Value $config_h_content
+
+    $unity_content = @"
+#define HB_EXTERN __declspec(dllexport)
+#include "config.h"
+#include "hb.hh"
+#include "hb-aat-layout.cc"
+#include "hb-aat-map.cc"
+#include "hb-blob.cc"
+#include "hb-buffer.cc"
+#include "hb-common.cc"
+#include "hb-face.cc"
+#include "hb-fallback-shape.cc"
+#include "hb-font.cc"
+#include "hb-map.cc"
+#include "hb-number.cc"
+#include "hb-ot-face.cc"
+#include "hb-ot-font.cc"
+#include "hb-ot-layout.cc"
+#include "hb-ot-map.cc"
+#include "hb-ot-math.cc"
+#include "hb-ot-shape.cc"
+#include "hb-ot-shape-fallback.cc"
+#include "hb-ot-shape-normalize.cc"
+#include "hb-ot-tag.cc"
+#include "hb-ot-var.cc"
+#include "hb-set.cc"
+#include "hb-shape.cc"
+#include "hb-shape-plan.cc"
+#include "hb-shaper.cc"
+#include "hb-static.cc"
+#include "hb-unicode.cc"
+#include "hb-subset.cc"
+#include "hb-subset-cff-common.cc"
+#include "hb-subset-cff1.cc"
+#include "hb-subset-cff2.cc"
+#include "hb-subset-input.cc"
+#include "hb-subset-plan.cc"
+#include "hb-ot-color.cc"
+#include "hb-ot-metrics.cc"
+#include "hb-ot-name.cc"
+#include "hb-ot-shaper-arabic.cc"
+#include "hb-ot-shaper-default.cc"
+#include "hb-ot-shaper-hangul.cc"
+#include "hb-ot-shaper-hebrew.cc"
+#include "hb-ot-shaper-indic.cc"
+#include "hb-ot-shaper-khmer.cc"
+#include "hb-ot-shaper-myanmar.cc"
+#include "hb-ot-shaper-syllabic.cc"
+#include "hb-ot-shaper-thai.cc"
+#include "hb-ot-shaper-use.cc"
+#include "hb-ot-shaper-vowel-constraints.cc"
+#include "hb-ucd.cc"
+"@
+
+    $unity_file = Join-Path $path_harfbuzz_build "harfbuzz_unity.cc"
+    Set-Content -Path $unity_file -Value $unity_content
+
+    # Compile unity file
+    $obj_file = "harfbuzz_unity.obj"
+    $command  = "cl.exe $compiler_args /c $unity_file /Fo$path_harfbuzz_build\$obj_file"
+
+    write-host "Compiling: $command"
+    invoke-expression $command
+
+    if ($LASTEXITCODE -ne 0) {
+        write-error "Compilation failed for unity build"
+        pop-location
+        return
+    }
+
+    push-location $path_harfbuzz_build
+
+    # Create library
+    if ($library_type -eq "static")
+    {
+        $lib_command = "lib.exe /OUT:harfbuzz.lib $obj_file"
+
+        write-host "Creating static library: $lib_command"
+        invoke-expression $lib_command
+
+        if ($LASTEXITCODE -ne 0) {
+            Write-Error "Static library creation failed"
+            pop-location
+            pop-location
+            return
+        }
+        $output_file = "harfbuzz.lib"
+    }
+    else
+    {
+        $linker_args = "/DLL", "/OUT:harfbuzz.dll"
+
+        if ($build_type -eq "debug") {
+            $linker_args += "/DEBUG"
+        }
+
+        $link_command = "link.exe $($linker_args -join ' ') $obj_file"
+
+        write-host "Creating shared library: $link_command"
+        invoke-expression $link_command
+
+        if ($LASTEXITCODE -ne 0) {
+            write-error "Shared library creation failed"
+            pop-location
+            pop-location
+            return
+        }
+        $output_file = "harfbuzz.dll"
+    }
+
+    pop-location # path_harfbuzz_build
+    pop-location # path_harfbuzz
+
+    # Copy files
+    $path_output = join-path $path_harfbuzz_build $output_file
+
+    if (test-path $path_output) {
+        copy-item -Path $path_output -Destination $path_win64 -Force
+        if ($library_type -eq "shared") {
+            $path_lib = join-path $path_harfbuzz_build "harfbuzz.lib"
+            if (test-path $path_lib) {
+                copy-item -Path $path_lib -Destination $path_win64 -Force
+            }
+        }
+    } else {
+        write-warning "Output file not found: $path_output"
+    }
+
+    write-host "Build completed and files copied to $path_win64"
+}
+# Build-RepoWithoutMeson
 
 function grab-binaries {
 	verify-path $script:path_lib
