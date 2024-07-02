d774d3873a78aab46bcf4c26c622247840a5efe0
Ed_ <edwardgz@gmail.com>
2024-07-02 13:29:18 -0400

    unity build works!

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -120,64 +120,58 @@ function Build-RepoWithoutMeson {
 #define HAVE_ROUND 1
 #define HB_NO_BITMAP 1
 #define HB_NO_CFF 1
+#define HB_NO_OT_FONT_CFF 1
 #define HB_NO_SUBSET_CFF 1
-#define HB_HAVE_SUBSET 1
-#define HB_HAVE_OT 1
+#define HB_HAVE_SUBSET 0
+#define HB_HAVE_OT 0
 #define HB_USER_DATA_KEY_DEFINE1(_name) extern HB_EXTERN hb_user_data_key_t _name
 "@
     set-content -Path (join-path $path_harfbuzz "config.h") -Value $config_h_content
 
     $unity_content = @"
 #define HB_EXTERN __declspec(dllexport)
+
+// base
 #include "config.h"
-#include "hb.hh"
 #include "hb-aat-layout.cc"
 #include "hb-aat-map.cc"
 #include "hb-blob.cc"
-#include "hb-buffer.cc"
 #include "hb-buffer-serialize.cc"
 #include "hb-buffer-verify.cc"
+#include "hb-buffer.cc"
 #include "hb-common.cc"
+
+//#include "hb-draw.cc"
+//#include "hb-paint.cc"
+//#include "hb-paint-extents.cc"
+
 #include "hb-face.cc"
 #include "hb-face-builder.cc"
 #include "hb-fallback-shape.cc"
 #include "hb-font.cc"
 #include "hb-map.cc"
 #include "hb-number.cc"
+#include "hb-ot-cff1-table.cc"
+#include "hb-ot-cff2-table.cc"
+#include "hb-ot-color.cc"
 #include "hb-ot-face.cc"
 #include "hb-ot-font.cc"
+
+#include "hb-outline.cc"
+#include "OT/Var/VARC/VARC.cc"
+
 #include "hb-ot-layout.cc"
 #include "hb-ot-map.cc"
 #include "hb-ot-math.cc"
-#include "hb-ot-shape.cc"
-#include "hb-ot-shape-fallback.cc"
-#include "hb-ot-shape-normalize.cc"
-#include "hb-ot-tag.cc"
-#include "hb-ot-var.cc"
-#include "hb-set.cc"
-#include "hb-shape.cc"
-#include "hb-shape-plan.cc"
-#include "hb-shaper.cc"
-#include "hb-static.cc"
-#include "hb-unicode.cc"
-#include "hb-subset.cc"
-#include "hb-subset-cff-common.cc"
-#include "hb-subset-cff1.cc"
-#include "hb-subset-cff2.cc"
-#include "hb-subset-instancer-iup.cc"
-#include "hb-subset-instancer-solver.cc"
-#include "hb-subset-input.cc"
-#include "hb-subset-plan.cc"
-#include "hb-ot-color.cc"
-#include "hb-ot-cff1-table.cc"
-#include "hb-ot-cff2-table.cc"
+#include "hb-ot-meta.cc"
 #include "hb-ot-metrics.cc"
 #include "hb-ot-name.cc"
-#include "hb-ot-shaper-indic-table.cc"
+
 #include "hb-ot-shaper-arabic.cc"
 #include "hb-ot-shaper-default.cc"
 #include "hb-ot-shaper-hangul.cc"
 #include "hb-ot-shaper-hebrew.cc"
+#include "hb-ot-shaper-indic-table.cc"
 #include "hb-ot-shaper-indic.cc"
 #include "hb-ot-shaper-khmer.cc"
 #include "hb-ot-shaper-myanmar.cc"
@@ -185,8 +179,35 @@ function Build-RepoWithoutMeson {
 #include "hb-ot-shaper-thai.cc"
 #include "hb-ot-shaper-use.cc"
 #include "hb-ot-shaper-vowel-constraints.cc"
+
+#include "hb-ot-shape-fallback.cc"
+#include "hb-ot-shape-normalize.cc"
+#include "hb-ot-shape.cc"
+#include "hb-ot-tag.cc"
+#include "hb-ot-var.cc"
+
+#include "hb-set.cc"
+#include "hb-shape-plan.cc"
+#include "hb-shape.cc"
+#include "hb-shaper.cc"
+#include "hb-static.cc"
+#include "hb-style.cc"
 #include "hb-ucd.cc"
-#include "graph/gsubgpos-context.cc"
+#include "hb-unicode.cc"
+
+// libharfbuzz-subset
+//#include "hb-subset-input.cc"
+//#include "hb-subset-cff-common.cc"
+//#include "hb-subset-cff1.cc"
+//#include "hb-subset-cff2.cc"
+//#include "hb-subset-instancer-iup.cc"
+//#include "hb-subset-instancer-solver.cc"
+//#include "hb-subset-plan.cc"
+//#include "hb-subset-repacker.cc"
+
+//#include "graph/gsubgpos-context.cc"
+
+//#include "hb-subset.cc"
 "@
     $unity_file = join-path $path_harfbuzz_build "harfbuzz_unity.cc"
     set-content -Path $unity_file -Value $unity_content
